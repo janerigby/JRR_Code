ed93cb00860e03488941a2a273944c72a6af2a62
Jane Rigby <Jane.Rigby@gmail.com>
2016-09-01 20:49:54 -0400

    upgraded mage_photospheric.py so it actually works and is readable

--- mage_photospheric.py
+++ mage_photospheric.py
@@ -28,51 +28,39 @@ S99fits = ('stack-A', 'S0004-0103', 'S0957+0509', 'S2111-0114', 'S0033+0242', 'S
 sorted_by_age = ('S0033+0242', 'rcs0327-knotE', 'rcs0327-knotG', 'S0108+0624', 'S0957+0509', 'Horseshoe', 'rcs0327-knotU', 'S2111-0114', 'S1429+1202', 'S0004-0103','S0900+2234', 'stack-A', 'S1527+0652', 'S1226+2152') # 'S1458-0023', 
 
 
-def local_s99_compare_manyspectra(list_S99files, line_cen, line_label, win, label, Ncol=1, vel_plot=False, mage_mode="reduction", LL=[], size=(8,8)) :
-    ''' Plot one transition, versus (wavelength or velocity?), for many MagE spectra.  Plot the S99 fit too (a second wave, fnu pair) too
-    line_cen:     rest-wavelength of line to plot, in Angstroms.
-    line_label:   label for that line
-    win:          window around that line.  If vel_plot, then units are km/s.  If not vel_plot, units are rest-frame Angstroms
-    vel_plot:     (optional, Boolean): If True, x-axis is velocity.  If False, x-axis is wavelength.
-    specs:        (optional) Pandas dataframe of MagE filenames & redshifts.'''
-    
-    Nspectra = len(list_S99files)
+def local_s99_compare_manyspectra(list_S99files, line_cen, line_label, win, label, Ncol=1, vel_plot=False, mage_mode="reduction", size=(8,8)) :
+    ''' Plot one transition for many MagE galaxies on a page, one page per transition.  Compare spectra and S99 fit
+    Wow, this runs REALLY slow. '''
     Nrow = int(np.ceil( (len(list_S99files)*1.0) / Ncol))  # Calculate how many rows to generate
     fig = plt.figure(figsize=size)
     plt.suptitle(label, fontsize=18)
 
     for ii, rootname in enumerate(list_S99files) :
         print rootname,
-        df = jrr.mage.open_S99_spectrum(rootname, 0.0)
+        (df, LL) = jrr.mage.open_S99_spectrum(rootname, 0.0)
         ax = fig.add_subplot(Nrow, Ncol, ii+1)
         if(vel_plot) : # Do velocity plot first, since already coded up.  Later, can add versus wavelength
-            vel = jrr.spec.convert_restwave_to_velocity(df.wave, line_cen)   # velocity in km/s
+            vel = jrr.spec.convert_restwave_to_velocity(df.rest_wave, line_cen)   # velocity in km/s
             in_window = vel.between(-1*win, win)
-            plt.step(vel[in_window], df.data_fnu[in_window], color=color1)
-            plt.step(vel[in_window], df.data_fnu_u[in_window], color=color2)
-            plt.step(vel[in_window], df.s99_fnu[in_window], color=color4)
+            plt.step(vel[in_window], df.rest_fnu_data[in_window]/df.rest_fnu_data_autocont[in_window], color=color1)
+            plt.step(vel[in_window], df.rest_fnu_data_u[in_window]/df.rest_fnu_data_autocont[in_window], color=color2)
+            plt.step(vel[in_window], df.rest_fnu_s99[in_window]/df.rest_fnu_s99_autocont[in_window], color=color4)
             plt.plot( (0., 0.), (0.0,2), color=color3, linewidth=2)  # plot tics at zero velocity
             plt.xlim(-1*win, win)
         else :
-            plt.annotate(rootname, (0.5,0.8), xycoords="axes fraction")
-            in_window = df.wave.between(line_cen - win, line_cen + win)
-            if(line_cen > 1600.0) :
-                plt.step(df.wave[in_window], df.data_fnu[in_window]   /np.median(df.data_fnu[in_window]), color=color1)
-                plt.step(df.wave[in_window], df.data_fnu_u[in_window]  /np.median(df.data_fnu[in_window]), color=color2)
-                plt.step(df.wave[in_window], df.s99_fnu[in_window]/np.median(df.s99_fnu[in_window]), color=color4)
-            else :
-                plt.step(df.wave[in_window], df.data_fnu[in_window]   /np.median(df.data_fnu[in_window]), color=color1)
-                plt.step(df.wave[in_window], df.data_fnu_u[in_window]  /np.median(df.data_fnu[in_window]), color=color2)
-                plt.step(df.wave[in_window], df.s99_fnu[in_window]/np.median(df.data_fnu[in_window]), color=color4)
+            plt.annotate(rootname, (0.5,0.85), xycoords="axes fraction")
+            in_window = df.rest_wave.between(line_cen - win, line_cen + win)
+            plt.step(df.rest_wave[in_window], df.rest_fnu_data[in_window]/df.rest_fnu_data_autocont[in_window], color=color1)
+            plt.step(df.rest_wave[in_window], df.rest_fnu_data_u[in_window]/df.rest_fnu_data_autocont[in_window], color=color2)
+            plt.step(df.rest_wave[in_window], df.rest_fnu_s99[in_window]/df.rest_fnu_s99_autocont[in_window], color=color4)
             plt.plot( (line_cen, line_cen), (0.0,2), color=color3, linewidth=2)  # plot tics at zero velocity
             plt.xlim(line_cen - win, line_cen + win)
         plt.ylim(0.0, 1.5)  # May need to change these limits
         jrr.mage.plot_linelist(LL, 0.0, True, False)  # plot the line IDs
-        
     if vel_plot :
         plt.xlabel("rest-frame velocity (km/s)")  
     else :
-        plt.xlabel(r'rest-frame wavelength($\AA$)')                                
+        plt.xlabel(r'rest-frame wavelength ($\rm \AA$)')                                
 #        fig.(hspace=0)
     return(0)
 
@@ -81,66 +69,52 @@ def local_s99_compare_manyspectra(list_S99files, line_cen, line_label, win, labe
 zz = 0.0  # stacked spectrum is already in rest frame wavelength
 xwin = 12. # +- velocity window (km/s) to consider a line
 Ncol = 4
+GALAXY_PER_PAGE = False   # False so I can debug the LINE_PER_PAGE part
+LINE_PER_PAGE = True
 
-# Open stacked spectrum and associated linelist
-linelist = line_path + "stacked.linelist"
-(LL, z_sys) = jrr.mage.get_linelist(linelist)
-(sp) = jrr.mage.open_stacked_spectrum(mage_mode)
-
-
-# What I need to do next.
-# All this below was written in a frenzy, and I didn't make it re-usable.  Also, 1 of the 2 functions above
-# is superceded by mage.boxplot_Nspectra, So I should switch to that.  The one difference is that above
-# does a local renormalization.  Add that functionality, either as an option in box_plot, (median of local window), or from autocont?
-# what to do about the second routine?  It's useful, but badly hardcoded?  use as is and move on?
- 
-plot_one_page_per_spectrum = False
-if plot_one_page_per_spectrum :
-    # This should generate same output as first run of loop below.  Check.
-    # Grab S99 fit to MagE Stack A
+if GALAXY_PER_PAGE :
+    print "STATUS:  Plotting the photospheric lines, using one page per galaxy."
     the_pdf = "S99-photospheric-bygalaxy.pdf"
     pp = PdfPages(the_pdf)  # output
-    S99 = jrr.mage.open_S99_spectrum("stack-A", 0.0)
-    alt_file = "magestack_byneb_ChisholmstackA_spectrum.txt"  # this is the spectrum that JChisholm fit
-    altsp = jrr.mage.open_stacked_spectrum(mage_mode, alt_file)
-    print "STATUS: Making an experimental plot of the photospheric absorption lines"
-    jrr.mage.boxplot_Nspectra((altsp.wave,S99.wave), (altsp.fnu,S99.fnu,), (altsp.fnu_u, S99.fnu_u), (0.0,0.0), line_label_p, line_center_p,  xwin, Ncol, LL, extra_label="StackA+S99 for sanity-checking", figsize=(16,16), vel_plot=False, plot_xaxis=True, ylims=(0.0,1.5))  # renorm=line_label_renorm,
-    pp.savefig()
-
     for rootname in S99fits :
-        df = jrr.mage.open_S99_spectrum(rootname)
-        local_boxplot_2spectra(df.wave, df.data_fnu, df.data_fnu_u, df.wave, df.s99_fnu, S99.s99_fnu*-0.01, line_label_p, line_center_p, redshift, redshift, xwin, Ncol, LL, rootname, (16,16), vel_plot=False, plot_xaxis=True)
+        print rootname, "Plotting photospheric lines"
+        (df, LL) = jrr.mage.open_S99_spectrum(rootname, 0.0)
+        jrr.plot.boxplot_Nspectra((df.rest_wave, df.rest_wave), (df.rest_fnu_data/df.rest_fnu_data_autocont, df.rest_fnu_s99/df.rest_fnu_s99_autocont), (df.rest_fnu_data_u/df.rest_fnu_data_autocont, ()), (0.0,0.0), line_label_p, line_center_p, xwin, Ncol, LL, figsize=(16,8),  vel_plot=False, plot_xaxis=True)
+        plt.suptitle(rootname, fontsize=18)
         pp.savefig()    
     pp.close()
 
-    # Repeat, but sorted by age
-    the_pdf = "MageES99/S99-photospheric-bygalaxy-sortbyage.pdf"
+if GALAXY_PER_PAGE :
+    print "STATUS:  Plotting the photospheric lines, using one page per galaxy, but now sorting by light-weighted age."
+    the_pdf = "S99-photospheric-bygalaxy-sortbyage.pdf"
     pp = PdfPages(the_pdf)  # output
     for rootname in sorted_by_age :
-        df = jrr.mage.open_S99_spectrum(rootname)
-        local_boxplot_2spectra(df.wave, df.data_fnu, df.data_fnu_u, df.wave, df.s99_fnu, S99.s99_fnu*-0.01, line_label_p, line_center_p, redshift, redshift, xwin, Ncol, LL, rootname, (16,16), False, True)
+        print rootname, "Plotting photospheric lines"
+        (df, LL) = jrr.mage.open_S99_spectrum(rootname, 0.0)
+        jrr.plot.boxplot_Nspectra((df.rest_wave, df.rest_wave), (df.rest_fnu_data/df.rest_fnu_data_autocont, df.rest_fnu_s99/df.rest_fnu_s99_autocont), (df.rest_fnu_data_u/df.rest_fnu_data_autocont, ()), (0.0,0.0), line_label_p, line_center_p, xwin, Ncol, LL, figsize=(16,8),  vel_plot=False, plot_xaxis=True)
+        plt.suptitle(rootname, fontsize=18)
         pp.savefig()    
     pp.close()
-
     
-    
-plot_one_page_per_line = True
-if plot_one_page_per_line :
-    the_pdf = "MageES99/S99-photospheric-bylines.pdf"
+#if LINE_PER_PAGE :
+if False:
+    print "STATUS:  Plotting the photospheric lines, one page per transition."
+    the_pdf = "S99-photospheric-bylines.pdf"
     pp = PdfPages(the_pdf)  # output
+#    for ii in range(0, 2) :  # for debugging
     for ii in range(0, len(line_center_p)) :
-        print "\n", line_label_p[ii], line_center_p[ii],
-        local_s99_compare_manyspectra(S99fits, line_center_p[ii], line_label_p[ii], xwin, line_label_p[ii], Ncol, size=(16,8), LL=LL)
+        print "\n", line_label_p[ii], line_center_p[ii], "***",
+        local_s99_compare_manyspectra(S99fits, line_center_p[ii], line_label_p[ii], xwin, line_label_p[ii], Ncol, size=(16,8))
         pp.savefig()
     pp.close()
 
-if plot_one_page_per_line :
-    the_pdf = "MageES99/S99-photospheric-bylines-sortbyage.pdf"
+if LINE_PER_PAGE :
+    print "STATUS:  Plotting the photospheric lines, one page per transition, now sorting by light-weighted age."
+    the_pdf = "S99-photospheric-bylines-sortbyage.pdf"
     pp = PdfPages(the_pdf)  # output
     for ii in range(0, len(line_center_p)) :
-        print "\n", line_label_p[ii], line_center_p[ii],
-        local_s99_compare_manyspectra(sorted_by_age, line_center_p[ii], line_label_p[ii], xwin, line_label_p[ii], Ncol, size=(16,8), LL=LL)
+        print "\n", line_label_p[ii], line_center_p[ii],"***",
+        local_s99_compare_manyspectra(sorted_by_age, line_center_p[ii], line_label_p[ii], xwin, line_label_p[ii], Ncol, size=(16,8))
         pp.savefig()
     pp.close()
     
-

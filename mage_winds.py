7f6bafbd6d20e25f5589e2991908b95860ecda7f
Jane Rigby <Jane.Rigby@gmail.com>
2016-10-31 17:00:05 -0400

    added S99 + median stack plots

--- mage_winds.py
+++ mage_winds.py
@@ -70,16 +70,24 @@ alt_file = "magestack_byneb_ChisholmstackA_spectrum.txt"  # this is the spectrum
 altsp = jrr.mage.open_stacked_spectrum(mage_mode, alt_file)
 plot_winds_neutral_stellar("MageES99/", (altsp.wave, S99.rest_wave), (altsp.X_avg/altsp.fnu_autocont, S99.rest_fnu_s99/S99.rest_fnu_s99_autocont), (altsp.X_sigma/altsp.fnu_autocont, S99.rest_fnu_s99*-0.01), (0.0, 0.0), vwin, Ncol, "Stack and S99 fit", LL, z_sys)
 
-# Same, but by age
-st1 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_younglt8Myr_spectrum.txt")
-st2 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_midage8to16Myr_spectrum.txt")
-st3 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_oldgt16Myr_spectrum.txt")
-plot_winds_neutral_stellar("MagEstack_byage/", (st1.wave, st2.wave, st3.wave), (st1.X_avg/st1.fnu_autocont,st2.X_avg/st2.fnu_autocont,st3.X_avg/st3.fnu_autocont), (st1.X_sigma/st1.fnu_autocont, st2.X_sigma/st2.fnu_autocont,st3.X_sigma/st3.fnu_autocont), (0.0, 0.0, 0.0), vwin, Ncol, "stacked by age: young (blue), middle (black), old (red)", LL, 0.0, colortab=('blue', 'black', 'red'))
+# Same, but for median spectrum.  Not sure how well the autocont will line up
+altsp = jrr.mage.open_stacked_spectrum(mage_mode, alt_file,  colfnu='X_median', colfnuu='X_jack_std')
+# Above should put the median in _fnu, and do autocont on it.  Let's see...
+plot_winds_neutral_stellar("MageES99_median/", (altsp.wave, S99.rest_wave), (altsp.X_median/altsp.fnu_autocont, S99.rest_fnu_s99/S99.rest_fnu_s99_autocont), (altsp.X_jack_std/altsp.fnu_autocont, S99.rest_fnu_s99*-0.01), (0.0, 0.0), vwin, Ncol, "Stack and S99 fit", LL, z_sys)
+                           
 
-# same, but by metallicity
-st1 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_lowZ_spectrum.txt")
-st2 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_highZ_spectrum.txt")
-plot_winds_neutral_stellar("MagEstack_byZ/", (st1.wave, st2.wave), (st1.X_avg/st1.fnu_autocont,st2.X_avg/st2.fnu_autocont), (st1.X_sigma/st1.fnu_autocont, st2.X_sigma/st2.fnu_autocont), (0.0, 0.0, 0.0), vwin, Ncol, "stacked by Z: lowZ (blue), highZ (black)", LL, 0.0, colortab=('blue', 'black'))
+### Obsolete, a bad alley to walk down.
+## Same, but by age
+#st1 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_younglt8Myr_spectrum.txt")
+#st2 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_midage8to16Myr_spectrum.txt")
+#st3 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_oldgt16Myr_spectrum.txt")
+#plot_winds_neutral_stellar("MagEstack_byage/", (st1.wave, st2.wave, st3.wave), (st1.X_avg/st1.fnu_autocont,st2.X_avg/st2.fnu_autocont,st3.X_avg/st3.fnu_autocont), (st1.X_sigma/st1.fnu_autocont, st2.X_sigma/st2.fnu_autocont,st3.X_sigma/st3.fnu_autocont), (0.0, 0.0, 0.0), vwin, Ncol, "stacked by age: young (blue), middle (black), old (red)", LL, 0.0, colortab=('blue', 'black', 'red'))
+#
+## same, but by metallicity
+#st1 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_lowZ_spectrum.txt")
+#st2 = jrr.mage.open_stacked_spectrum(mage_mode, "magestack_bystars_highZ_spectrum.txt")
+#plot_winds_neutral_stellar("MagEstack_byZ/", (st1.wave, st2.wave), (st1.X_avg/st1.fnu_autocont,st2.X_avg/st2.fnu_autocont), (st1.X_sigma/st1.fnu_autocont, st2.X_sigma/st2.fnu_autocont), (0.0, 0.0, 0.0), vwin, Ncol, "stacked by Z: lowZ (blue), highZ (black)", LL, 0.0, colortab=('blue', 'black'))
+
 
 print "STATUS:  Making same figure as Heckman et al. Figure 1 but for our sample"
 line_label_Heck  = ('S II 1260', 'C II 1334', 'Si III 1206', 'Si IV 1393', 'N II 1084')  

83c0658b72243b05e7b0d8222e46daa83f895f6d
janerigby <jane.rigby@gmail.com>
2017-08-10 14:47:27 -0400

    added redshifts to schedulability plot

--- ERS_schedulable.py
+++ ERS_schedulable.py
@@ -6,8 +6,8 @@ import datetime
 def midpoint_date(dateA, dateB) :
     return (dateA + (dateB - dateA)/2)
 
-def draw_ERS_box(ax, ERSwin, ymin, ymax) :
-    ax.fill( [ERSwin[0], ERSwin[1], ERSwin[1], ERSwin[0]], [ymin, ymin, ymax, ymax], 'grey', alpha=0.2, edgecolor='k')
+def draw_ERS_box(ax, ERSwin, ymin, ymax, color='grey') :
+    ax.fill( [ERSwin[0], ERSwin[1], ERSwin[1], ERSwin[0]], [ymin, ymin, ymax, ymax], color, alpha=0.2, edgecolor='k')
     return(0)
 
 def adjust_date_axis(ax) :
@@ -42,6 +42,7 @@ win7 = (datetime.date(2019, 7,24), datetime.date(2020, 2,16))
 #
 windows = (win1,    win2, win3,  win4,  win5,      win7  )
 names   = ("S1723", noL,  "S1226", noL, "SPT0346", "SPT0418")
+namesz  = ("S1723 (z=1.32)", noL,  "S1226 (z=2.92)", noL, "SPT0346 (z=5.66)", "SPT0418 (z=4.22)")
 offsets = (0,       0,      1,     1,    2,        3)
 color   = ('b',     'b',   'b',    'b',  'b',      'b')
 
@@ -71,6 +72,8 @@ awin16 =(datetime.date(2019,12, 1), datetime.date(2020, 1,20))
 
 awindows = (awin1,        awin2, awin3,        awin4, awin5,        awin6,         awin7,     awin8,     awin9,  awin10,      awin11, awin12,  awin13, awin14, awin15,   awin16)  
 anames   = ("SPT2134-50", noL,   "SPT2147-50", noL,   'SPT0532-50', 'SPT0532-50',  'RCS0327', 'RCS0327', noL,    'PlanckArc', noL,    "S1110", noL,    "S1050", "S1050", noL)     
+anamesz  = ("SPT2134 (z=2.78)", noL,   "SPT2147 (z=3.76)", noL,   'SPT0532 (z=3.40)', 'SPT0532 (z=3.40)',  'RCS0327 (z=1.70)', 'RCS0327 (z=1.70)', noL,    'PlanckArc (z=2.37)', noL,    "S1110 (z=2.48)", noL,    "S1050 (z=3.63)", "S1050 (z=3.63)", noL)     
+
 aoffsets = (0,            0,     .5,            .5,     1,            1,             1.5,      1.5,      1.5,    2,           2,      2.5,      2.5,    3,      3,        3)       
 acolor   = ('g',          'g',   'g',          'g',   'g',          'g',           'g',       'g',       'g',    'g',         'g',    'g',     'g',    'g',    'g',      'g')     
 
@@ -84,22 +87,29 @@ xmax = datetime.date(2020, 3,1)
 fig = plt.figure(figsize=(12,3))
 ax1 = fig.add_subplot(1, 2, 1)
 for ii, thiswin in enumerate(windows) :
-    ax1.plot(thiswin, (offsets[ii], offsets[ii]), label=names[ii], color=color[ii], lw=3)
-    print "Primary target", names[ii], "window duration:", thiswin[1] - thiswin[0]
-    if names[ii] != noL :
-        ax1.annotate(names[ii], xy=(midpoint_date(*thiswin), 0.05+offsets[ii]), xycoords='data', ha='center')
+    ax1.plot(thiswin, (offsets[ii], offsets[ii]), label=namesz[ii], color=color[ii], lw=3)
+    print "Primary target", namesz[ii], "window duration:", thiswin[1] - thiswin[0]
+    if namesz[ii] != noL :
+        ax1.annotate(namesz[ii], xy=(midpoint_date(*thiswin), 0.05+offsets[ii]), xycoords='data', ha='center')
 ax1.set_xlim(xmin, xmax)
 ax1.set_ylim(ymin, ymax)
-draw_ERS_box(ax1, ERSwin, ymin, ymax)    
+draw_ERS_box(ax1, ERSwin, ymin, ymax)
+
+# Draw some alternative boxes, temp:
+draw_ERS_box(ax1, (datetime.date(2019, 7, 1), datetime.date(2019, 11, 30)), ymin, ymax, color='yellow')
+draw_ERS_box(ax1, (datetime.date(2019, 10, 1), datetime.date(2020, 2, 28)), ymin, ymax, color='pink')
+
+
+
 ax1.set_title("High priority targets")
 adjust_date_axis2(ax1) 
 
 ax2 = fig.add_subplot(1, 2, 2)
 for ii, thiswin in enumerate(awindows) :
-    print "Alt target", anames[ii], "window duration:", thiswin[1] - thiswin[0]
-    ax2.plot(thiswin, (aoffsets[ii], aoffsets[ii]), label=anames[ii], color=acolor[ii], lw=3)
-    if anames[ii] != noL :
-        ax2.annotate(anames[ii], xy=(midpoint_date(*thiswin), 0.05+aoffsets[ii]), xycoords='data', ha='center')
+    print "Alt target", anamesz[ii], "window duration:", thiswin[1] - thiswin[0]
+    ax2.plot(thiswin, (aoffsets[ii], aoffsets[ii]), label=anamesz[ii], color=acolor[ii], lw=3)
+    if anamesz[ii] != noL :
+        ax2.annotate(anamesz[ii], xy=(midpoint_date(*thiswin), 0.05+aoffsets[ii]), xycoords='data', ha='center')
 ax2.set_title("Alternative targets")
 ax2.set_xlim(xmin, xmax)
 ax2.set_ylim(ymin, ymax)
--- jwst_redshifts_ERS.py
+++ jwst_redshifts_ERS.py
@@ -3,6 +3,8 @@ import numpy as np
 names = ("SGAS1723", "RCS0327", "PSZ1G311.65-18.48", "SGAS1110+6459", "SGAS1226p2152", "SGAS1050p0017")
 zz = np.array((1.329279, 1.7034, 2.369,                    2.481, 2.925257, 3.6253))
 
+waves = np.array((0.4861, 0.6563))
+
 #NIRSPEC GRATING & FILTER CHOICES
 G140_F100 = np.array((0.97, 1.89))
 G235_F170 = np.array((1.66, 3.17))
@@ -11,6 +13,12 @@ the_gratings  = (G140_F100, G235_F170, G396_F290)
 grating_names = ("G140_F100", "G235_F170", "G396_F290")
 
 for ii, name in enumerate(names) :
-    print name, "at z =",zz[ii], 
+    print name, "at z =",zz[ii]
+    print "HB, Ha:",  (1.0 +zz[ii]) * waves
     for jj, grating in enumerate(the_gratings) :
         print "   ", grating / (1.0+zz[ii]) , grating_names[jj]
+
+print "Idea redshifts for each grating:"
+for jj, grating in enumerate(the_gratings) :
+    print grating_names[jj], np.average(grating) / np.average(waves) - 1.0
+    

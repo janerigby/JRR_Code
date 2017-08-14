df2a3f161374e9f1e273edb5cdde19bd0852492d
Jane Rigby <Jane.Rigby@gmail.com>
2017-08-14 08:47:19 -0400

    moving SPT targets from primary to secondar

--- ERS_schedulable.py
+++ ERS_schedulable.py
@@ -40,11 +40,6 @@ win4 = (datetime.date(2019,12,13), datetime.date(2020, 2, 6))
 win5 = (datetime.date(2019, 7, 3), datetime.date(2020, 1,29))
 win7 = (datetime.date(2019, 7,24), datetime.date(2020, 2,16))
 #
-windows = (win1,    win2, win3,  win4,  win5,      win7  )
-names   = ("S1723", noL,  "S1226", noL, "SPT0346", "SPT0418")
-namesz  = ("S1723 (z=1.32)", noL,  "S1226 (z=2.92)", noL, "SPT0346 (z=5.66)", "SPT0418 (z=4.22)")
-offsets = (0,       0,      1,     1,    2,        3)
-color   = ('b',     'b',   'b',    'b',  'b',      'b')
 
 # Alternate SPT targets in case of later start to Cycle 1
 awin1 = (datetime.date(2019, 4,22), datetime.date(2019, 6,28))
@@ -70,15 +65,23 @@ awin14 =(datetime.date(2019, 1, 1), datetime.date(2019, 1,19))
 awin15 =(datetime.date(2019, 4,19), datetime.date(2019, 6,10))
 awin16 =(datetime.date(2019,12, 1), datetime.date(2020, 1,20))
 
-awindows = (awin1,        awin2, awin3,        awin4, awin5,        awin6,         awin7,     awin8,     awin9,  awin10,      awin11, awin12,  awin13, awin14, awin15,   awin16)  
-anames   = ("SPT2134-50", noL,   "SPT2147-50", noL,   'SPT0532-50', 'SPT0532-50',  'RCS0327', 'RCS0327', noL,    'PlanckArc', noL,    "S1110", noL,    "S1050", "S1050", noL)     
-anamesz  = ("SPT2134 (z=2.78)", noL,   "SPT2147 (z=3.76)", noL,   'SPT0532 (z=3.40)', 'SPT0532 (z=3.40)',  'RCS0327 (z=1.70)', 'RCS0327 (z=1.70)', noL,    'PlanckArc (z=2.37)', noL,    "S1110 (z=2.48)", noL,    "S1050 (z=3.63)", "S1050 (z=3.63)", noL)     
 
-aoffsets = (0,            0,     .5,            .5,     1,            1,             1.5,      1.5,      1.5,    2,           2,      2.5,      2.5,    3,      3,        3)       
-acolor   = ('g',          'g',   'g',          'g',   'g',          'g',           'g',       'g',       'g',    'g',         'g',    'g',     'g',    'g',    'g',      'g')     
+# New primary targets, after SPT folks shifted to lower redshift
+windows = (win1,                win2, win3,                win4,  awin1,                 awin2, awin3,                 awin4)
+namesz  = ("S1723+34 (z=1.32)", noL,  "S1226+21 (z=2.92)", noL,   "SPT2134-50 (z=2.78)", noL,   "SPT2147-50 (z=3.76)", noL)   
+offsets = (0,                   0,    2,                   2,     1,                     1,     3,                     3)   
+color   = ('b',                'b',   'b',                 'b',   'r',                  'r',    'r',                   'r')    
+
+# New alternate targets, moving two highest-z sources from primary to alternate
+awindows = ( win5,                win7,               awin5,              awin6,               awin7,              awin8,             awin9,  awin10,               awin11, awin12,           awin13, awin14,           awin15,           awin16)  
+anamesz  = ( "SPT0346 (z=5.66)",  "SPT0418 (z=4.22)", 'SPT0532 (z=3.40)', 'SPT0532 (z=3.40)', 'RCS0327 (z=1.70)', 'RCS0327 (z=1.70)', noL,    'PlanckArc (z=2.37)', noL,    "S1110 (z=2.48)", noL,    "S1050 (z=3.63)", "S1050 (z=3.63)", noL)     
+aoffsets = ( 3,                   2.5,                1.5,                1.5,                 0.0,                0.0,               0.0,    0.5,                  0.5,      1.0,            1.0,     2.0,              2.0,             2.0)       
+acolor   = ( 'r',                 'r',                'r',                'r',                 'b',                'b',               'b',    'b',                  'b',    'b',              'b',    'b',              'b',              'b')     
 
 
 ERSwin = (datetime.date(2019, 4, 1), datetime.date(2019, 8, 31))
+annotate_xpos1 = datetime.date(2019,5,1)
+annotate_xpos2 = datetime.date(2019,9,10)
 ymin = -0.5
 ymax = 3.3
 xmin = datetime.date(2019, 3,1)
@@ -90,14 +93,15 @@ for ii, thiswin in enumerate(windows) :
     ax1.plot(thiswin, (offsets[ii], offsets[ii]), label=namesz[ii], color=color[ii], lw=3)
     print "Primary target", namesz[ii], "window duration:", thiswin[1] - thiswin[0]
     if namesz[ii] != noL :
-        ax1.annotate(namesz[ii], xy=(midpoint_date(*thiswin), 0.05+offsets[ii]), xycoords='data', ha='center')
+        #ax1.annotate(namesz[ii], xy=(midpoint_date(*thiswin), 0.05+offsets[ii]), xycoords='data', ha='center')
+        ax1.annotate(namesz[ii], xy=(annotate_xpos1, 0.05+offsets[ii]), xycoords='data', ha='left')
 ax1.set_xlim(xmin, xmax)
 ax1.set_ylim(ymin, ymax)
 draw_ERS_box(ax1, ERSwin, ymin, ymax)
 
 # Draw some alternative boxes, temp:
-draw_ERS_box(ax1, (datetime.date(2019, 7, 1), datetime.date(2019, 11, 30)), ymin, ymax, color='yellow')
-draw_ERS_box(ax1, (datetime.date(2019, 10, 1), datetime.date(2020, 2, 28)), ymin, ymax, color='pink')
+#draw_ERS_box(ax1, (datetime.date(2019, 7, 1), datetime.date(2019, 11, 30)), ymin, ymax, color='yellow')
+#draw_ERS_box(ax1, (datetime.date(2019, 10, 1), datetime.date(2020, 2, 28)), ymin, ymax, color='pink')
 
 
 
@@ -109,8 +113,9 @@ for ii, thiswin in enumerate(awindows) :
     print "Alt target", anamesz[ii], "window duration:", thiswin[1] - thiswin[0]
     ax2.plot(thiswin, (aoffsets[ii], aoffsets[ii]), label=anamesz[ii], color=acolor[ii], lw=3)
     if anamesz[ii] != noL :
-        ax2.annotate(anamesz[ii], xy=(midpoint_date(*thiswin), 0.05+aoffsets[ii]), xycoords='data', ha='center')
-ax2.set_title("Alternative targets")
+        #ax2.annotate(anamesz[ii], xy=(midpoint_date(*thiswin), 0.05+aoffsets[ii]), xycoords='data', ha='center')
+        ax2.annotate(anamesz[ii], xy=(annotate_xpos2, 0.05+aoffsets[ii]), xycoords='data', ha='left')
+ax2.set_title("Alternate targets")
 ax2.set_xlim(xmin, xmax)
 ax2.set_ylim(ymin, ymax)
 draw_ERS_box(ax2, ERSwin, ymin, ymax)

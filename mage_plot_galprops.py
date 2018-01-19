373a32fcbba963f9579f8ebb7d7bbff29060f979
janerigby <jane.rigby@gmail.com>
2018-01-19 17:51:56 -0500

    plotting Zneb vs Zstellar for megasaura

--- mage_plot_galprops.py
+++ mage_plot_galprops.py
@@ -3,19 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import jrr
 
+# Compare the derived nebular metallicities and stellar metallicities
+# Cosmic Eye gets plotted twice, because it has 2 different nebular Zs
+
+
+def add_other_Zneb_CosmicEye(df):
+    starkZ = 8.6;  starkZu = 0.25 # starkZu is a guess
+    altZneb =  10**(starkZ - 8.69)
+    altZnebsig = 10**(starkZ + starkZu - 8.69) - 10**(starkZ  - 8.69)
+    plt.scatter(altZneb, df.loc['Cosmic~Eye']['Z_S99'], color='b')
+    plt.errorbar(altZneb, df.loc['Cosmic~Eye']['Z_S99'], xerr=altZnebsig, ls='none', color='k')
+    print "DEBUGGING", altZneb, altZnebsig
+    return(0)
 
 # Get results from mage_calc_galprops.py
 galprops =  pandas.read_table("mage_galproperties.txt", delim_whitespace=True, comment="#")
+galprops.set_index('label', inplace=True, drop=False)
 plt.ion()
 
 hasZneb   = galprops.loc[galprops['sigZ']>0]
 uplimZneb = galprops.loc[galprops['sigZ']==-99]
 
-plt.scatter(hasZneb['Zneb'], hasZneb['Z_S99'], color='blue')
-plt.errorbar(hasZneb['Zneb'], hasZneb['Z_S99'], xerr=hasZneb['sigZ'], ls='none')
-plt.scatter(uplimZneb['Zneb'], uplimZneb['Z_S99'], color='red')
-plt.errorbar(uplimZneb['Zneb'], uplimZneb['Z_S99'], xerr=0.05, ls='none', xuplims=np.ones_like(uplimZneb['Z_S99']))
+plt.scatter(hasZneb['Zneb'], hasZneb['Z_S99'], color='b')
+plt.errorbar(hasZneb['Zneb'], hasZneb['Z_S99'], xerr=hasZneb['sigZ'], ls='none', color='k')
+plt.scatter(uplimZneb['Zneb'], uplimZneb['Z_S99'], color='b')
+plt.errorbar(uplimZneb['Zneb'], uplimZneb['Z_S99'], xerr=0.03, ls='none', xuplims=np.ones_like(uplimZneb['Z_S99']), color='k')
 jrr.plot.annotate_from_dataframe(galprops, xcol='Zneb', ycol='Z_S99', text='label')
+add_other_Zneb_CosmicEye(hasZneb)
 
 plt.xlabel('Nebular Z (fraction of solar)')
 plt.ylabel('S99 Z (fraction of solar)')

98b88ec09b61277bac8b4cc1ba6651addfec7dfd
Jane Rigby <Jane.Rigby@gmail.com>
2018-02-01 09:51:27 -0500

    got new spectra included

--- mage_calc_MW_reddening.py
+++ mage_calc_MW_reddening.py
@@ -1,5 +1,6 @@
-import jrr
+import pandas
 import numpy as np
+import jrr
 import query_argonaut
 import astropy.io.ascii
 from astropy.coordinates import SkyCoord
@@ -20,12 +21,20 @@ result = query_argonaut.query(c.ra.value.tolist(), c.dec.value.tolist(), coordsy
 df['EBV_Green2015'] = (np.array(list(result.values()))).flatten()
 print df[['Source name', 'EBV_Green2015']]
 
-
 planckarc = SkyCoord(ra="15:50:04.57", dec="-78:10:59.5", unit=(u.hourangle, u.deg))   # Coords for S1 in Table 1 of Dahle et al. 2016
 result_planck = query_argonaut.query(planckarc.ra.value, planckarc.dec.value, coordsys='equ', mode='sfd')  # query only takes lists as input
 print "E(B-V) for the Planck arc is", result_planck
-
-
 # Then by-hand emacs-ing to get these EBV values in to spectra-filenames-redshifts.txt
+
+
+
+# Repeat for the Friends-of-Megasaura supplemental sample
+coords_file = "/Volumes/Apps_and_Docs/SCIENCE/Lensed-LBGs/Observing/Where-pointed-MagE/Friends_of_Mage/friendsofMegasaura_summary_observations.txt"
+colnames = ("filename", "UTdate", "UTtime_start", "UTtime_stop", "RA", "DEC", "texp", "sec", "slit", "angle")
+df =  pandas.read_table(coords_file, delim_whitespace=True, comment="#", names=colnames)
+c = SkyCoord(ra=df['RA'] , dec=df['DEC'], unit=(u.hourangle, u.deg))  # Convert from segidecimal to decimal
+result = query_argonaut.query(c.ra.value.tolist(), c.dec.value.tolist(), coordsys='equ', mode='sfd')  # query only takes lists as input
+df['EBV_Green2015'] = (np.array(list(result.values()))).flatten()
+print df[['filename', 'EBV_Green2015']]
 
 

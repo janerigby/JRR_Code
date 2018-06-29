ac08385e606106c1a1c1b41f9de3968f05a765d3
janerigby <jane.rigby@gmail.com>
2018-06-29 16:51:57 -0400

    adding labels to emission lines

--- grism_fit_spectra_v4.py
+++ grism_fit_spectra_v4.py
@@ -31,8 +31,6 @@ def error_unknown_grism(which_grism) :
     raise Exception('ERROR: Cannot understand which_grism', which_grism)
     return(0)
 
-
-
 def get_redshift(which_gal) :
     if which_gal == 'S1723' :
         zz = 1.331366
@@ -46,14 +44,22 @@ def get_redshift(which_gal) :
 def get_line_wavelengths(which_grism) :
     if which_grism == "G141" :
         restwaves   = np.array((4862.683,  4960.295,  5008.240,  5877.59,  6310.,            6549.85,  6564.61,  6585.28, 6725.))
+        yaxcoords   =         [0.2,        -1,       0.96,        0.1,     0.1,               -1,       0.55,      -1,       0.2] 
+        plotnames   =         [r'H$\beta$', '',    '[O III]',    'He I',  '[O I]+[S III]',   '',      r'H$\alpha$', '',       '[S II]']
         linenames   =         ['Hbeta',   '[O~III]', '[O~III]',  'He~I',  '[O~I]+[S~III]',  '[N~II]', 'Halpha', '[N~II]', '[S~II]']
-        #                       0             1            2         3          4             5           6           7         8  
+        #                       0             1            2         3          4             5           6           7         8
+
+    # Need to do the same cleanup of plotnames for G102 that I did for G141 above.  Then remake the figures, and move on.
+    # Also, get it to use figsize!! ****** stopped here ***
     elif which_grism == "G102" :
         restwaves = np.array((3727.092,   3729.875,   3869.86,  3890.151,  3968.593,   3971.195,   4025.,        4102.892, 4341.684, 4364.436, 4472.7,  4687.02,   4741.4489, 4862.683))
+        yaxcoords =         [  0.9,       -1,           0.2,       0.2,      0.2,          0.2,      0.2,        0.2,        0.2,      0.2,     0.2,    0.2,         0.2,     0.2]
+        plotnames =         ['[O II]',    '',        '[Ne~III]', 'H8+HeI', '[Ne~III]', 'Heps',     'HeI+HeII',  'Hdelt',  'Hgam',  '[O~III]', 'He~I', 'He~II',    '[Ar~IV]', 'Hbeta']
         linenames =         ['[O~II]',    '[O~II]',  '[Ne~III]', 'H8+HeI', '[Ne~III]', 'Heps',     'HeI+HeII',  'Hdelt',  'Hgam',  '[O~III]', 'He~I', 'He~II',    '[Ar~IV]', 'Hbeta']
         #                       0             1            2         3          4         5           6           7          8        9        10        11         12           13
     else : error_unknown_grism(which_grism)
-    return(restwaves, linenames)
+    label_df = pandas.DataFrame({'restwave' : restwaves, 'yaxcoord' : yaxcoords, 'plotname' : plotnames})  # For convenience, make dataframe of labels to plot
+    return(restwaves, linenames, label_df)
 
 # SHould have done the below functions with just a single "params" var that gets unpcked, as in v=params.valuesdict().  See https://lmfit.github.io/lmfit-py/parameters.html
 # Also, probably should have done all this in astropy, which has similar functionality. Moving on.
@@ -62,7 +68,7 @@ def fitfunc_G141(wave, zz, morph_broad, f0, f1, f2, f3, f4, f5, f6, f7, f8) : # 
     # Parameters are the FLUX of each Gaussian, = a*c*sqrt(2pi).  Since sigmas are fixed, its clearer to have params be FLUXES.
     # This version fits a global redshift zz, and allows no wavelength miscalibration between lines.
     fluxes          = np.array((f0,        f1,        f2,        f3,         f4,          f5,          f6,       f7,  f8))
-    (restwaves, linenames) = get_line_wavelengths("G141")
+    (restwaves, linenames, label_df) = get_line_wavelengths("G141")
     return (fitfunc_eithergrism(wave, zz, morph_broad, restwaves, fluxes, which_grism='G141'))
 
 def fitfunc_G141_waveoff(wave, zz, morph_broad, f0, f1, f2, f3, f4, f5, f6, f7, f8, d0, d1, d2, d3, d4, d5, d6, d7, d8) : # A custom G141 fitting function
@@ -70,20 +76,20 @@ def fitfunc_G141_waveoff(wave, zz, morph_broad, f0, f1, f2, f3, f4, f5, f6, f7, 
     # This version lets each line have a dwave offset, to deal w relative wavelength calibration problems in grism spectra
     fluxes          = np.array((f0,        f1,        f2,        f3,      f4,      f5,      f6,       f7, f8))
     dwaves       = np.array((d0, d1, d2, d3, d4, d5, d6, d7, d8))
-    (restwaves, linenames) = get_line_wavelengths("G141")
+    (restwaves, linenames, label_df) = get_line_wavelengths("G141")
     return (fitfunc_eithergrism(wave, zz, morph_broad, restwaves, fluxes, which_grism='G141', dwaves=dwaves))
 
 def fitfunc_G102(wave, zz, morph_broad, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13) : # A custom G102 fitting function
     # parameters are the FLUX of each Gaussian, = a*c*sqrt(2pi).  Since sigmas are fixed, it is much easier to have params be FLUXES.  Easier to report, too
     fluxes    = np.array((f0,         f1,        f2,        f3,       f4,       f5,        f6,       f7,       f8,       f9,       f10,      f11,      f12, f13))
-    (restwaves, linenames) = get_line_wavelengths("G102")
+    (restwaves, linenames, label_df) = get_line_wavelengths("G102")
     return (fitfunc_eithergrism(wave, zz, morph_broad, restwaves, fluxes, which_grism='G102'))
 
 def fitfunc_G102_waveoff(wave, zz, morph_broad, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13) : 
     # parameters are the FLUX of each Gaussian, = a*c*sqrt(2pi).  Since sigmas are fixed, it is much easier to have params be FLUXES.  Easier to report, too
     fluxes    = np.array((f0,         f1,        f2,        f3,       f4,       f5,        f6,       f7,       f8,       f9,       f10,      f11,      f12, f13))
     dwaves       = np.array((d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13))
-    (restwaves, linenames) = get_line_wavelengths("G102")
+    (restwaves, linenames, label_df) = get_line_wavelengths("G102")
     return (fitfunc_eithergrism(wave, zz, morph_broad, restwaves, fluxes, which_grism='G102', dwaves=dwaves))
 
 def fitfunc_eithergrism(wave, zz, morph_broad, restwaves, fluxes, which_grism, dwaves=()) :  # This part is the same for either grism
@@ -185,30 +191,40 @@ def parse_filename(grism_filename) :
     mydict = { 'gname': m.group(1), 'descrip': m.group(2), 'roll': m.group(3),  'grating': m.group(4), 'suffix': m.group(5)}
     return(mydict)
 
-def plot_results(df_data, fit_result, func2fit, title, grism_info, show_initial_fit=False, scalefactor=1.0, units=1.0) :
+def finergrid_result(grism_info, fit_result, outfile) :
+    #The fit result is on the same coarse wavelength array as the data.  Make finer fit for plotting, and save it.
+    xfine = np.linspace(grism_info['x1'], grism_info['x2'], 1000)
+    fit_df = pandas.DataFrame(xfine, columns=('wave',))
+    fit_df['bestfit'] = func2fit(xfine, **fit_result.values)
+    fit_df.to_csv(outfile, float_format='%.5E', index=False)
+    return(xfine)
+
+def plot_results(df_data, fit_result, func2fit, xfine, title, grism_info, show_initial_fit=False, scalefactor=1.0, units=1.0) :
     print "   Plotting"
     ax = df_data.plot(x='wave', y='flam_contsub_scaled', color='black', linestyle='steps-mid', lw=1.5, legend=False)
     df_data.plot(x='wave', y='flam_u_scaled', color='grey', ax=ax, legend=False)
     if show_initial_fit : plt.plot(df_data['wave'], fit_result.init_fit, color='orange', label='init fit')
-#    plt.plot(subset['wave'], fit_result.best_fit, color='blue', label='best fit')
-    xfine = np.linspace(grism_info['x1'], grism_info['x2'], 1000)
     plt.plot(xfine, func2fit(xfine, **fit_result.values), color='blue', label='best fit')
     plt.xlabel(r"observed wavelength ($\AA$)")
-    plt.ylabel(r"continuum-subtracted  $f_\lambda$ ("+str(1./scalefactor) + " " + units + ")")
+    #plt.ylabel(r"continuum-subtracted  $f_\lambda$ ("+str(1./scalefactor) + " " + units + ")")
+    plt.ylabel(r"$f_\lambda$ ("+str(1./scalefactor) + " " + units + ")")
     pretty_title = re.sub("wcontMWdr.txt ", "", re.sub("_", " ", title))
     plt.title(pretty_title, position=(0.5, 0.9), fontsize=9)# position=(0.7, 0.9),)
     #plt.suptitle(title, y=0.995, fontsize=10)
     plt.xlim(grism_info['x1'], grism_info['x2'])
-    (restwaves, linenames) = get_line_wavelengths(which_grism)
+    (restwaves, linenames, label_df) = get_line_wavelengths(which_grism)
     if 'd0' in fit_result.params.keys() :
         offsets = np.array([fit_result.params[x].value for x in fit_result.params.keys() if re.match('d', x)])
         plt.scatter(restwaves*(1. + fit_result.best_values['zz']) + offsets, np.zeros_like(restwaves)-0.1, marker='+', color='k')
     else :    plt.scatter(restwaves*(1. + fit_result.best_values['zz']), np.zeros_like(restwaves)-0.1, marker='+', color='k')
     ax2 = ax.twiny()
     ax2.set_xlim( grism_info['x1']/(1. + fit_result.best_values['zz']), grism_info['x2']/(1. + fit_result.best_values['zz']))
-    ax2.set_xlabel(r"rest-frame vacuum wavelength ($\AA$)", labelpad=10)
+    ax2.set_xlabel(r"rest-frame wavelength ($\AA$)", labelpad=10)  # vacuum
     ax.xaxis.set_minor_locator(AutoMinorLocator(4))
-    ax2.xaxis.set_minor_locator(AutoMinorLocator(4))    
+    ax2.xaxis.set_minor_locator(AutoMinorLocator(4))
+    label_df['ydata'] = label_df['yaxcoord'] *  ax2.axes.get_ylim()[1]
+    print "DEBUGGING\n", label_df.head()
+    jrr.plot.annotate_from_dataframe(label_df, xcol='restwave', ycol='ydata', text='plotname', xytext=(0,0), ha='center')
     plt.show()
 
 def  convert_lmfitresults_2df(LMresult, parnames, sigoff, grism_info, restwaves, linenames):
@@ -251,7 +267,7 @@ infile = 'S1723_G141_grism2process.txt'
 #infile = 'S2340_G102_grism2process.txt'  # CHANGE THIS.  Keep format
 #infile = 'S2340_G141_grism2process.txt'  
 
-figsize = (8,4)
+figsize = (12,4)
 scalefactor = 1E17 # Scale everything by scalefactor, to avoid numerical weirdness in LMFIT
 units = "erg/s/cm^2"
 guess_morphbroad = 1.5
@@ -273,7 +289,7 @@ Method 2 works great for almost all S2340 spectra. For a few at low SNR it stall
 m = re.search('(\S+)_(\S+)_(\S+)', infile)   # Formart of input file tells us which galaxy, which grism
 which_gal   = m.group(1)
 which_grism = m.group(2)
-(restwaves, linenames) = get_line_wavelengths(which_grism)
+(restwaves, linenames, label_df) = get_line_wavelengths(which_grism)
 df = pandas.read_table(infile, comment="#", delim_whitespace=True)  # Read the list of files to process
 
 # Gather info needed for fitting
@@ -281,16 +297,18 @@ zz = get_redshift(which_gal)
 grism_info = jrr.grism.get_grism_info(which_grism) 
 pp = PdfPages("grism_fitspectra_"+which_gal+"_"+which_grism+".pdf")
 
-for row in df.itertuples() :
+for row in df[0:1].itertuples() :
     specfile = basename(row.filename)
     print row.filename
     subdir = re.split("/", row.filename)[-3] + "/" # This should be the dir, like 1Dsum
     if not exists(subdir):  makedirs(subdir)
     tweak_wav = row.tweak_wav
     specfile_dict = parse_filename(specfile)
-    outfile0 = subdir + re.sub('.txt', '.fitreport', specfile)
+    outfile0 = subdir + re.sub('.txt', '.fitreport',   specfile)
     outfile1 = subdir + re.sub('.txt', '_meth1.fitdf', specfile)
-    outfile2 = subdir + re.sub('.txt', '_meth2.fitdf', specfile)
+    outfile2 = subdir + re.sub('.txt', '_meth1.model', specfile)
+    outfile3 = subdir + re.sub('.txt', '_meth2.fitdf', specfile)
+    outfile4 = subdir + re.sub('.txt', '_meth2.model', specfile)
     f = open(outfile0, 'w')  
     header = "# Fitting HST grism spectra with grism_fit_spectra_v4.py\n# FILENAME "+ specfile
     header += "\n# FILENAME_WPATH " + row.filename + "\n# WHICH_GAL " + which_gal
@@ -321,13 +339,14 @@ for row in df.itertuples() :
     zz_fit = result1.best_values['zz']
     if print_fitreports : print result1.fit_report()
     f.write(result1.fit_report())
-    plot_label = subdir + " " + specfile + " fit 1"
-    plot_results(subset, result1, func2fit, plot_label, grism_info, show_initial_fit=show_initial_fit, scalefactor=scalefactor, units=units)
-    pp.savefig(bbox_inches='tight', pad_inches=0.1)
     df1 = convert_lmfitresults_2df(result1, parnames, sigoff, grism_info, restwaves, linenames)
     df1.to_csv('/tmp/foo', float_format='%.3f', index=False)    
     jrr.util.put_header_on_file('/tmp/foo', header + supplemental_header(result1), outfile1)
     if check4zero_errorbars(result1, parnames) : print "####### WARNING: errorbars were zero! ####### "
+    plot_label = subdir + " " + specfile + " fit 1"
+    xfine = finergrid_result(grism_info, result1, outfile2)
+    plot_results(subset, result1, func2fit, xfine, plot_label, grism_info, show_initial_fit=show_initial_fit, scalefactor=scalefactor, units=units)
+    pp.savefig(bbox_inches='tight', pad_inches=0.1, figsize=figsize)
        
     if row.tweak_wav :  # If input file requests a second fit, w line centroids allowed to very:
         #print "   ******************************************************"
@@ -341,13 +360,14 @@ for row in df.itertuples() :
         result2  = mymodel.fit(subset['flam_contsub_scaled'], locked_params, wave=subset['wave'], verbose=True)  # fitting is done here
         if print_fitreports : print result2.fit_report()
         f.write(result2.fit_report())
-        plot_label = subdir + " " + specfile + " fit 2"
-        plot_results(subset, result2, func2fit, plot_label, grism_info, show_initial_fit=show_initial_fit, scalefactor=scalefactor, units=units)
-        pp.savefig(bbox_inches='tight', pad_inches=0.1)
         df2 = convert_lmfitresults_2df(result2, parnames, sigoff, grism_info, restwaves, linenames)
         df2.to_csv('/tmp/foo', float_format='%.3f', index=False)
-        jrr.util.put_header_on_file('/tmp/foo', header + supplemental_header(result2), outfile2)
+        jrr.util.put_header_on_file('/tmp/foo', header + supplemental_header(result2), outfile3)
         if check4zero_errorbars(result2, parnames) : print "####### WARNING: errorbars were zero! ####### "
+        xfine = finergrid_result(grism_info, result2, outfile4)
+        plot_label = subdir + " " + specfile + " fit 2"
+        plot_results(subset, result2, func2fit, xfine, plot_label, grism_info, show_initial_fit=show_initial_fit, scalefactor=scalefactor, units=units)
+        pp.savefig(bbox_inches='tight', pad_inches=0.1)
     f.close()
 
 

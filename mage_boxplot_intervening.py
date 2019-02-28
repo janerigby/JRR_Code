9701532247caa218e45b2fbf3249a77891e9ef8e
Rigby <Jane.Rigby@gmail.com>
2019-02-28 13:56:07 -0500

    Cleaner flow of script, more details in title, both batch1 and batch23

--- mage_boxplot_intervening.py
+++ mage_boxplot_intervening.py
@@ -8,43 +8,41 @@ from matplotlib.backends.backend_pdf import PdfPages
 mage_mode = "released"
 # Make a boxplot for each isolated Intervening system in the MagE Megasaura spectra
 
-
-homedir = expanduser("~")
-batch = "batch23"
-
-if   batch == "batch1" :
-    doublet_file = homedir + "/Dropbox/MagE_atlas/Contrib/Intervening/Doublet_search/Results_16Feb2018/found_doublets_SNR4_JRRedit.txt"
-elif batch == "batch23" :
-    doublet_file = homedir + "/Dropbox/MagE_atlas/Contrib/Intervening/Doublet_search/Results_21Feb2019/found_doublets_batch3_SNR4_JRRedit.txt"
-
-trans_file = homedir + "/Dropbox/MagE_atlas/Linelists/MINE/interven.lst"
-trans_list = pandas.read_table(trans_file, delim_whitespace=True, comment="#", names=("wave", "lab1", "lab2", "f", "d", "type"))
-trans_list['label'] = trans_list.lab1 + "_" + trans_list.lab2.astype('str')
-
-def make_boxplots_of_doublets(doublet_file=doublet_file) :
+def make_boxplots_of_doublets(doublet_file) :
     doublet_list = pandas.read_table(doublet_file, delim_whitespace=True, comment="#")
     #foo = doublet_list[0:5]
     for row in doublet_list.itertuples():
     #for row in foo.itertuples():  # TEMP, only run a few doublets, for debugging
         if row.flag == "y" :
-            print "Working on intervening absorber at z=", row.zz, " in ", row.gal, row.pointing
             (sp, resoln, dresoln, LL, zz_syst) = jrr.mage.wrap_open_spectrum(row.gal, mage_mode, addS99=True)  # load spectrum
             lims = jrr.spec.get_waverange_spectrum(sp)
             # Only plot the transitions that are covered by the spectrum, at intervening absorber redshift row.zz
             transin = trans_list[trans_list.wave.between(lims[0]/(1.+row.zz), lims[1]/(1.+row.zz))] 
             transin.reset_index(inplace=True, drop=True)
-            jrr.plot.boxplot_Nspectra( (sp.wave,), (sp.fnu/sp.fnu_autocont,), (sp.fnu_u/sp.fnu_autocont,), (row.zz,), transin.label, transin.wave, win=2000, Ncol=1, LL=LL, vel_plot=True)
-            extra_label = row.gal + " " + row.pointing + " Intervening absorber at z="+ str(np.round(row.zz, decimals=5))
+            jrr.plot.boxplot_Nspectra( (sp.wave,), (sp.fnu/sp.fnu_autocont,), (sp.fnu_u/sp.fnu_autocont,), (row.zz,), transin.label, transin.wave, win=2000, Ncol=1, LL=LL, vel_plot=True)            
+            extra_label = row.gal + " " + row.pointing + " Intervening absorber at z="+ str(np.round(row.zz, decimals=4)) + " (" + row.flag + ")"
+            print "Working on " + extra_label
             plt.suptitle(extra_label, fontsize=20)
             pp.savefig()
-# boxplot_Nspectra(thewaves, thefnus, thedfnus, thezs, line_label, line_center, win=2000, Ncol=1, LL=(), extra_label="",figsize=(8,16), vel_plot=True, plot_xaxis=True, ymax=(), colortab=False, verbose=True, drawunity=False) :
-            
     return(0)
 
-the_pdf = "intervening_doublets_boxplots_batch3.pdf"
-pp = PdfPages(the_pdf)
-foo =  make_boxplots_of_doublets()
-pp.close()
+
+batches = ('batch1', 'batch23')
+doubletfiles = {}
+doubletfiles['batch1']  = expanduser("~") + "/Dropbox/MagE_atlas/Contrib/Intervening/Doublet_search/Results_16Feb2018/found_doublets_SNR4_JRRedit.txt"
+doubletfiles['batch23'] = expanduser("~") + "/Dropbox/MagE_atlas/Contrib/Intervening/Doublet_search/Results_21Feb2019/found_doublets_batch3_SNR4_JRRedit.txt"
+
+
+trans_file = expanduser("~") + "/Dropbox/MagE_atlas/Linelists/MINE/interven.lst"
+trans_list = pandas.read_table(trans_file, delim_whitespace=True, comment="#", names=("wave", "lab1", "lab2", "f", "d", "type"))
+trans_list['label'] = trans_list.lab1 + "_" + trans_list.lab2.astype('str')
+
+for batch in batches :
+    the_pdf = "intervening_doublets_boxplots_" + batch + ".pdf"
+    pp = PdfPages(the_pdf)
+    foo =  make_boxplots_of_doublets(doubletfiles[batch])
+    pp.close()
+    
 
 '''         plt.clf()
             

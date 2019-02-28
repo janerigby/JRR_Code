96a2fde38ed028471737a526e824bc2a6a44f292
Rigby <Jane.Rigby@gmail.com>
2019-02-28 17:09:41 -0500

    getting actual pointings spectra to plot

--- mage_boxplot_intervening.py
+++ mage_boxplot_intervening.py
@@ -2,6 +2,7 @@ import jrr
 #import ayan
 import numpy as np
 import pandas
+import string
 from os.path import expanduser
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -10,17 +11,22 @@ mage_mode = "released"
 
 def make_boxplots_of_doublets(doublet_file) :
     doublet_list = pandas.read_table(doublet_file, delim_whitespace=True, comment="#")
-    #foo = doublet_list[0:5]
+    foo = doublet_list[0:5]
     for row in doublet_list.itertuples():
-    #for row in foo.itertuples():  # TEMP, only run a few doublets, for debugging
-        if row.flag == "y" :
-            (sp, resoln, dresoln, LL, zz_syst) = jrr.mage.wrap_open_spectrum(row.gal, mage_mode, addS99=True)  # load spectrum
+#    for row in foo.itertuples():  # TEMP, only run a few doublets, for debugging
+        if "y" in row.flag or "p" in row.flag :
+            shortname = ""
+            if row.pointing == 'main' : shortname = row.gal
+            else :
+                shortname = string.replace(row.gal + '_' + row.pointing, "_knot", "-")
+            print shortname, row.gal, row.pointing
+            (sp, resoln, dresoln, LL, zz_syst) = jrr.mage.wrap_open_spectrum(shortname, mage_mode, addS99=True)  # load spectrum
             lims = jrr.spec.get_waverange_spectrum(sp)
             # Only plot the transitions that are covered by the spectrum, at intervening absorber redshift row.zz
             transin = trans_list[trans_list.wave.between(lims[0]/(1.+row.zz), lims[1]/(1.+row.zz))] 
             transin.reset_index(inplace=True, drop=True)
             jrr.plot.boxplot_Nspectra( (sp.wave,), (sp.fnu/sp.fnu_autocont,), (sp.fnu_u/sp.fnu_autocont,), (row.zz,), transin.label, transin.wave, win=2000, Ncol=1, LL=LL, vel_plot=True)            
-            extra_label = row.gal + " " + row.pointing + " Intervening absorber at z="+ str(np.round(row.zz, decimals=4)) + " (" + row.flag + ")"
+            extra_label = row.gal + " " + row.pointing + ",  intervening absorber z="+ str(np.round(row.zz, decimals=4)) + " (" + row.flag + ")"
             print "Working on " + extra_label
             plt.suptitle(extra_label, fontsize=20)
             pp.savefig()
@@ -33,7 +39,8 @@ doubletfiles['batch1']  = expanduser("~") + "/Dropbox/MagE_atlas/Contrib/Interve
 doubletfiles['batch23'] = expanduser("~") + "/Dropbox/MagE_atlas/Contrib/Intervening/Doublet_search/Results_21Feb2019/found_doublets_batch3_SNR4_JRRedit.txt"
 
 
-trans_file = expanduser("~") + "/Dropbox/MagE_atlas/Linelists/MINE/interven.lst"
+trans_file = expanduser("~") + "/Dropbox/MagE_atlas/Linelists/MINE/interven_short.lst"
+#trans_file = expanduser("~") + "/Dropbox/MagE_atlas/Linelists/MINE/interven.lst"  # Longer list, kinda hard to see
 trans_list = pandas.read_table(trans_file, delim_whitespace=True, comment="#", names=("wave", "lab1", "lab2", "f", "d", "type"))
 trans_list['label'] = trans_list.lab1 + "_" + trans_list.lab2.astype('str')
 

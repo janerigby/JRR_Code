4e1643b75b36c5107e1e0de5ed44247e9d6a66de
Rigby <Jane.Rigby@gmail.com>
2019-02-28 13:27:18 -0500

    pseudocode  for Michael

---
+++ grism_Ews_from_fluxes.py
@@ -0,0 +1,16 @@
+import jrr
+import glob
+from os.path import basename, expanduser, exists
+
+# Set this up.  Kludgy
+thisdir = "/Volumes/Apps_and_Docs/jrrigby1/Dropbox/Grism_S1723/WFC3_fit_1Dspec/1Dsum/"
+
+myfiles = [ basename(x) for x in glob.glob(thisdir + "*.fitdf") ]
+for infile in myfiles :
+    head = jrr.util.read_header_from_file(infile, comment="#")
+    print head
+
+
+
+
+#jrr.util.put_header_on_file(infile, head, outfile)

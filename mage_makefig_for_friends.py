9bc0e02948562cc5324dd45d31d3f8b94341707d
janerigby <jane.rigby@gmail.com>
2020-03-09 09:00:23 -0400

    Cycle 28 proposal figure

--- mage_makefig_for_friends.py
+++ mage_makefig_for_friends.py
@@ -15,9 +15,9 @@ mage_mode = "released"
 plt.ion()
 
 batch23 = jrr.mage.organize_labels('batch23')   # Adding Apr2018 and Aug2018 runs
-labels = ('PSZ0441', 'SPT0310', 'SPT2325', 'SPT0356', 'SPT0142')  # Just the 5 targets of this proposal
-Z      = (0.10,       0.23,       0.27,     0.22,       0.23 )  # John didnt fit these, ro at least, they're not in hot stars table 3
-age    = (19.3,      15.7,        9.2,      8.6,        10.1)
+labels = ('PSZ0441', 'SPT0310', 'SPT2325', 'SPT0356', 'SPT0142', 'planckarc_h9')  #
+Z      = (0.10,       0.23,       0.27,     0.22,       0.23,     0.55 )  
+age    = (19.3,       15.7,        9.2,      8.6,        10.1,     2.9)
 age_dict = dict(list(zip(labels, age)))
 Z_dict =   dict(list(zip(labels, Z)))
 
@@ -30,9 +30,11 @@ for ii, key in enumerate(list(sp.keys())) :
 
 #jrr.mage.plot_linelist(LL[key], z_systemic=zz_sys[key],  restframe=True)
 
+home= expanduser("~")
+figdir = home + "/Dropbox/SGAS-shared/PROPOSALS/Hubble/Cycle27/Friends_o_Meg/Figure/"
+plotorder = ('PSZ0441',  'SPT0310', 'SPT0142', 'SPT2325', 'SPT0356', 'planckarc_h9')   # from bottom to top
+need_HST = ('PSZ0441', 'SPT0142', 'SPT0356')  
 
-figdir = "/Volumes/Apps_and_Docs/jrrigby1/Dropbox/SGAS-shared/PROPOSALS/Hubble/Cycle27/Friends_o_Meg/Figure/"
-plotorder = ('PSZ0441',  'SPT0310', 'SPT0142', 'SPT2325', 'SPT0356')   # from bottom to top
 filenames =  [ basename(x) for x in glob.glob(figdir + "*-fit.txt") ]
 colnames=('wave', 'obsflux', 'errflux', 'S99fit')
 df = {}
@@ -40,41 +42,74 @@ df = {}
 #    myre = re.compile('(\S+)-sb99-fit.txt')
 #    key = myre.match(filename).groups()[0]
 
+what_to_plot = "CIV"
+
+if what_to_plot == "CII" :
+    xy1 = 1328 ; xy2 = 1332
+    scalef = 2
+    xlim = (1315,1355)
+    labelshift = 11
+elif what_to_plot == "CIV":
+    xy1 = 1530 ; xy2 = 1536  
+    scalef = 2
+    xlim = (1530,1570)
+    labelshift = 0
+elif what_to_plot == "SiII" :
+    xy1 = 1250 ; xy2 = 1255
+    scalef =2
+    xlim = (1240,1280)
+    labelshift = 5
+
+
 fig = plt.figure(figsize=(11,12))
 ax = fig.add_subplot(1, 1, 1)
-nudgear = (1, 1, 1, 1, 1, 1)
-scalef = 1.2
 nudge  = 1.1
 key = from_meg[-1]
-plt.plot(sp[key].rest_wave, (sp[key].rest_fnu / sp[key].rest_fnu_autocont) - nudge, label=key, linestyle='steps-mid', color='k')  # Plot S21226
+plt.plot(sp[key].rest_wave, (sp[key].rest_fnu / sp[key].rest_fnu_autocont) - nudge, label=key, linestyle='steps-mid')  # Plot S21226
 plt.plot(sp[key].rest_wave, (sp[key].rest_fnu_s99model / sp[key].rest_fnu_autocont) - nudge, label='_nolegend_', color='b')
-plt.annotate(key +", age=26 Myr, Z=0.34",  xy=(1530, 0 - nudge), xycoords='data', fontsize=14)
+plt.annotate(key +", age=26 Myr, Z=0.34",  xy=(xy1-labelshift, 0 - nudge), xycoords='data', fontsize=14)
 for ii, key in enumerate(plotorder):
+    if key in need_HST : lw =2 
+    else : lw =1
+    if key == "planckarc_h9" : altlab = "Sunburst Arc"
+    else :                     altlab = key
+    plotlab = altlab + ", age=" + str(age_dict[key]) + " Myr, Z=" + str(Z_dict[key])
     filename = key + '-sb99-fit.txt'
     df[key] = pandas.read_table(figdir + filename, delim_whitespace=True, comment="#", names=colnames)
     df[key]['obsflux'].loc[df[key]['obsflux'].lt(-10)] = np.nan
-    df[key]['tempy'] = df[key]['obsflux']  /  df[key]['obsflux'][df[key].wave.between(1530,1536)].median()  # renormalize for plot    
-    df[key]['tempS99'] = df[key]['S99fit'] /  df[key]['obsflux'][df[key].wave.between(1530,1536)].median()  # renormalize for plot    
-    plt.plot(df[key]['wave'], df[key]['tempy'] + ii*scalef, label=key, linestyle='steps-mid')
+    df[key]['tempy'] = df[key]['obsflux']  /  df[key]['obsflux'][df[key].wave.between(xy1,xy2)].median()  # renormalize for plot    
+    df[key]['tempS99'] = df[key]['S99fit'] /  df[key]['obsflux'][df[key].wave.between(xy1,xy2)].median()  # renormalize for plot
+    if key in need_HST:
+        plt.plot(df[key]['wave'], df[key]['tempy'] + ii*scalef, label=key, linestyle='steps-mid', lw=1.8, color='k')
+        plt.annotate(plotlab, xy=(xy1-labelshift,ii*scalef+0.), xycoords='data', fontsize=18, fontweight='bold')
+    else :
+        plt.plot(df[key]['wave'], df[key]['tempy'] + ii*scalef, label=key, linestyle='steps-mid', lw=1.5)
+        plt.annotate(plotlab, xy=(xy1-labelshift,ii*scalef+0.5), xycoords='data', fontsize=14)
     plt.plot(df[key]['wave'], df[key]['tempS99'] + ii*scalef, color='b', label='_nolegend_')
-    plotlab = key + ", age=" + str(age_dict[key]) + " Myr, Z=" + str(Z_dict[key]) 
-    plt.annotate(plotlab, xy=(1530,ii*scalef+0.5), xycoords='data', fontsize=14)
+
 key = from_meg[0]  # Plot RCS0327-E
-nudge = ii +  2.5
-plt.plot(sp[key].rest_wave, (sp[key].rest_fnu / sp[key].rest_fnu_autocont) + nudge, label=key, linestyle='steps-mid', color='k')
+nudge = ii +  7
+plt.plot(sp[key].rest_wave, (sp[key].rest_fnu / sp[key].rest_fnu_autocont) + nudge, label=key, linestyle='steps-mid')
 plt.plot(sp[key].rest_wave, (sp[key].rest_fnu_s99model / sp[key].rest_fnu_autocont) + nudge, label='_nolegend_', color='b')
-plt.annotate(key +", age=2.5 Myr, Z=0.24",  xy=(1530, nudge+0.3), xycoords='data', fontsize=14)
-plt.ylim(-1.2,8.4)
-plt.xlim(1530,1570)
-#plt.xlim(1500,1600)
+plt.annotate(key +", age=2.5 Myr, Z=0.24",  xy=(xy1-labelshift, nudge+0.3), xycoords='data', fontsize=14)
+plt.ylim(-1.2,14)
+plt.xlim(xlim)
 #plt.xlim(1350,1450)
 plt.xlabel(r'rest wavelength ($\AA$)',  fontsize=18)
 plt.ylabel('scaled flux', fontsize=18)
+plt.plot((1260.4221,1260.4221), (-20,20), color='grey', lw=2) # Draw a vert line at SiII
+plt.plot((1335.7077, 1335.7077), (-20,20), color='grey', lw=2) # Draw a vert line at C II
+plt.plot((1548.195, 1548.195), (-20,20), color='grey', lw=2) # Draw a vert line at CIV
+plt.plot((1550.770, 1550.770), (-20,20), color='grey', lw=2) # Draw a vert line at CIV
+plt.annotate(what_to_plot, xy=(0.6,0.01), xycoords='axes fraction', fontsize=20, fontweight='bold',horizontalalignment='right')
 
 # Bigger x axis, suppress y axis
-matplotlib.rc('xtick', labelsize=16) 
-matplotlib.rc('ytick', labelsize=16)
+matplotlib.rc('xtick', labelsize=18) 
+matplotlib.rc('ytick', labelsize=18)
 plt.yticks([])
+plt.tight_layout()
+fig.savefig(what_to_plot + "_C28extended.pdf")
+
 
 #handles, labels = ax.get_legend_handles_labels()
 #ax.legend(reversed(handles), reversed(labels), title='Line', loc='upper left')

3c9e150532eec6b0a40b7f5f10892c067a4a98c5
janerigby <jane.rigby@gmail.com>
2020-08-20 14:10:54 -0400

    Loaded spectral dispersions and calculated limiting line flux for an unresolved line, rather than flux density

--- jwst_sens.py
+++ jwst_sens.py
@@ -3,9 +3,12 @@ from builtins import range
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.ticker import ScalarFormatter, FuncFormatter
-import glob
-from os.path import basename, expanduser, exists
-import pandas 
+from os.path import expanduser, exists
+import re
+import pandas
+import jrr
+from   astropy.io import fits
+from astropy.table import Table          
 
 #pandir = '/Users/jrrigby1/MISSIONS/JWST/Sens/pandeia_sensitivities_1.3/'
 pandir = '/Users/jrrigby1/MISSIONS/JWST/Sens/pandeia_sensitivities_1.5/'
@@ -16,7 +19,7 @@ def load_photometry() :
     phot_codes = {'NIRSpec' : 0, 'NIRCam': 1, 'MIRI' : 2, 'HST' : 4, 'WISE' : 5, 'Spitzer' : 6, 'gemini' :10 , 'herschel' : 11, 'sofia' : 12}
     #13=alma(cycle0), 14=alma(finished), 15=VLA, 16=EVLA
     names = ('name', 'wave', 'limfnu', 'code')                
-    phot_df = pandas.read_table(pandir + '../jwst-phot.dat', comment="#", delim_whitespace=True, usecols=[0,1,2,3], names=names)
+    phot_df = pandas.read_csv(pandir + '../jwst-phot.dat', comment="#", delim_whitespace=True, usecols=[0,1,2,3], names=names)
     phot_df['wave'] =  pandas.to_numeric(phot_df['wave'])
     return(phot_df, phot_codes)
 
@@ -25,12 +28,6 @@ def load_spectroscopy() :
     # so that I can convert from continuum sensitivity to line sensitivity
     return(0)
 
-
-def force_axisticks_linear(whichaxis) :
-    for axis in whichaxis :  #ax.yaxis]:
-        formatter = FuncFormatter(lambda y, _: '{:.16g}'.format(y))
-        axis.set_major_formatter(formatter)
-       
 def pretty_plot() :
     plt.xlabel("wavelength (micron)", fontsize=fs3)
     plt.ylabel('limiting flux density (Jy)', fontsize=fs3)
@@ -39,28 +36,14 @@ def pretty_plot() :
     plt.xscale('log')
     plt.tight_layout()
 
-
-        
+    
+df_sens = jrr.instruments.load_Klaus_sensitivities(pandir)          # Klaus's sensitivities from Pandeia
+df_disp = jrr.instruments.load_Klaus_dispersions(pandir=None)  # Klaus's spectral resolutions from Pandeia
+print("I think I imported all of Klaus's files:", list(df_sens.keys()))
 plt.ion()
-# Load Klaus's sensitivities
-filenames = [ basename(x) for x in glob.glob(pandir + '*npz')]
-pan = {} # empty dictionary of pandeia files (in weird numpy format)
-df = {}  # empty dict of dataframes
-for filename in filenames :
-    (rootname, suffix) = filename.split('.')
-    pan[rootname] = np.load(pandir + filename, allow_pickle=True)
 
-    uglyintermed = []
-    for ii in range(0, len(list(pan[rootname].items()))) :
-        uglyintermed.append( list(pan[rootname].items())[ii][1].flatten())  #oh god it burns
-    tempdf = pandas.DataFrame(data=uglyintermed)
-    #tempdf = pandas.DataFrame(data=[pan[rootname].items()[0][1].flatten(), pan[rootname].items()[1][1].flatten(), pan[rootname].items()[2][1].flatten(), pan[rootname].items()[3][1].flatten(), pan[rootname].items()[4][1].flatten()])
-    df[rootname] = tempdf.transpose()
-    df[rootname].columns = list(pan[rootname].keys())
 
-print("I think I imported all of Klaus's files:", list(df.keys()))
-
-plt.clf()
+plt.close("all")
 fig, ax = plt.subplots(figsize=(8.5,6))
 plt.xticks(fontsize=fs3)
 plt.yticks(fontsize=fs3)
@@ -70,8 +53,8 @@ modes  = ['nircam_sw', 'nircam_lw', 'miri_imaging']
 labels = ['JWST NIRCam', '_nolegend_', 'JWST MIRI']
 # Plot imaging
 for ii, mode in enumerate(modes) :
-    thekey = mode + '_sensitivity'
-    subset = df[thekey].loc[df[thekey]['configs'].astype(str).str.contains('w')]  # Only Wide (broadband) filters  
+    thismode = mode + '_sensitivity'
+    subset = df_sens[thismode].loc[df_sens[thismode]['configs'].astype(str).str.contains('w')]  # Only Wide (broadband) filters  
     plt.plot(subset['wavelengths'], subset['lim_fluxes']*1E-3, label=labels[ii], color='red', marker='o', linestyle='-')
 
 # Harvesting Jane's sensitivities
@@ -92,21 +75,54 @@ plt.text(2.0, 3E-9, "JWST NIRCam", color='red', fontsize=fs3)
 plt.text(11., 3E-7, "JWST MIRI", color='red', fontsize=fs3)
 plt.text(8.8, 4.5E-5, "Spitzer", color='orange', fontsize=fs3)
 pretty_plot()
-force_axisticks_linear((ax.xaxis,))
+jrr.plot.force_axisticks_linear((ax.xaxis,))
 plt.show()
 fig.savefig(pandir + 'phot_plot.pdf')
 
-# Now, plot spectroscopy 
+# Now, plot spectroscopy
+# If we can grab the spectral resolution, can plot limiting sensitivity to an unresolved line (in erg/s/cm^2).  What a spectroscopist wants.
 fig2, ax2 = plt.subplots()
-modes  = ['nirspec_msa', 'miri_mrs'] #, 'miri_lrs'] # 'nircam_wfgrism']
-for ii, mode in enumerate(modes) :
-    thekey = mode + '_sensitivity'
-    for index, row in df[thekey].iterrows() :     # Ah, shit, need go row by row.
-        plt.plot(row.wavelengths, row.lim_fluxes*1E-3, color='red')
-plt.xlim(0.5, 30)
-plt.ylim(3E-9, 1E-2)
-pretty_plot()
-force_axisticks_linear((ax2.xaxis,))
-#
+modes  = ['nirspec_msa', 'nirspec_ifu', 'miri_mrs'] #, 'miri_lrs'] # 'nircam_wfgrism']
+
+# make a smaller dict of sensitivities for the spectroscopic modes only
+df_specsens = {}
+specmodes = ['nirspec_fs','nirspec_ifu', 'nirspec_msa',  'miri_mrs']# 'nircam_wfgrism', 'miri_lrs',  'niriss_wfss','niriss_soss'
+for specmode in specmodes:
+    df_specsens[specmode + "_sensitivity"] = df_sens[specmode + "_sensitivity"].copy(deep=True)
+for ii, mode in enumerate(specmodes) :
+    thismode = mode + '_sensitivity'
+    df_specsens[thismode]['mode'] = mode
+    df_specsens[thismode]['R'] = df_specsens[thismode]['wavelengths'] * 0  # initialize
+    df_specsens[thismode]['PSSL'] = df_specsens[thismode]['wavelengths'] * 0  # initialize
+    for index, row in df_specsens[thismode].iterrows() :     # Go row by row, for each grating, order, etc
+        #plt.plot(row.wavelengths, row.lim_fluxes*1E-3, color='red')
+        if 'miri' in thismode :    disp_key = 'jwst_miri_' + row.aperture + '-' + row.disperser
+        elif 'nircam' in thismode: disp_key = 'jwst_nircam'
+        else:                      disp_key = 'jwst_' + re.split('_', thismode)[0] + '_' + row.disperser
+        print("DEBUGGING", disp_key)
+        rebinnedR = jrr.spec.rebin_spec_new(df_disp[disp_key]['WAVELENGTH'], df_disp[disp_key]['R'], row.wavelengths)
+        df_specsens[thismode].loc[index, 'R'] = rebinnedR
+    # Now, need to convert to limiting line flux for an unresolved line.
+    # Eqn 2.7 of the Spitzer IRS manual:  PSSL = 3.0E-15 PSSC / (R lambda), where PSSC is in units of mJy, and lambda is in microns.  PSSL is in W/m^2
+        df_specsens[thismode].loc[index, 'PSSL'] = row.lim_fluxes * 3.0E-15 / ( row.R  * row.wavelengths) * 1E3  #The last 1E3 converts to cgs
+        plt.plot(row.wavelengths, row.PSSL)
+        plt.yscale('log')
+        plt.xlabel("wavelength (micron)")
+        plt.ylabel("limiting unresolved line flux (erg/s/cm^2)")
+bigspec_df = pandas.concat(df_specsens)
+
+pandeia_ver = "v1.5.0"
+outfile = 'pandeia_lineflux_sensitivities_' + pandeia_ver + '.readme'  
+header_text =  '# PSSL (erg/s/cm^2) is the line flux detectable for a spectrally unresolved line, point source, at SNR=10, in 1E4s, from Pandeia ' + pandeia_ver + '\n'
+header_text += '# Adapting Eqn 2.7 of the Spitzer IRS manual:  PSSL (erg/s/cm^2) = 3.0E-15 PSSC / (R lambda) * 1E3,\n'
+header_text += '# where PSSC is in units of mJy, lambda is in microns, and PSSL is in erg/s/cm^2\n'
+header_text += '# In Python pandas, read this file as:  df = pandas.read_csv(\'' + outfile + '\', index_col=[0,1], comment=\'#\')\n'
+header_text += '# Jane.Rigby@nasa.gov, 20 Aug. 2020.\n#\n'
+jrr.util.put_header_on_file('/dev/null', header_text, outfile)
+bigspec_df.to_pickle(re.sub('readme', 'pcl', outfile))
+
+#nirspec_setttings = [x for x in df_disp.keys() if 'nirspec' in x] 
+    
+
 
 

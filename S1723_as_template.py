c185bd3b5faf9066f27d1905931cdac37f016cd2
janerigby <jane.rigby@gmail.com>
2020-10-21 14:11:15 -0400

    found the bug that made the output spectrum scaled wrong.

--- S1723_as_template.py
+++ S1723_as_template.py
@@ -62,33 +62,33 @@ def load_s1110_Johnson_Tab4() :
 def make_synthetic_spectrum_based_on_S1723(zz, fwhm_kms=100, scaleby=1.0) :  # fwhm_kms is assumed line width, fwhm, in km/s.  scaleby is factor to scale fluxes
     # Convenient wrapper function to load the S1723 template, and use it to generate a simulated spectrum
     # Load the S1723 lineflux measurements into a well-behaved pandas dataframe.  Uses the local copy which has duplicate line measurements commented out
-    s1723_df = load_s1723_lineflux_template() 
+    df_s1723 = load_s1723_lineflux_template() 
     c_kms = constants.c.to('km/s').value   # Someday I'll trust astropy.units
-    s1723_df['sigma'] = (fwhm_kms  / c_kms ) * gaussian_fwhm_to_sigma * s1723_df['wave']  # linewidths to use
-    detected_lines =  s1723_df[s1723_df['detected']]             
-    print("Will scale the fluxes of S1723 by a factor of", np.round(scaleby, 4))
+    df_s1723['sigma'] = (fwhm_kms  / c_kms ) * gaussian_fwhm_to_sigma * df_s1723['wave']  # linewidths to use
+    detected_lines =  df_s1723[df_s1723['detected']].copy(deep=True)             #solving pandas' SettingWithCopyWarning  
+    print("Will scale the fluxes of S1723 by a factor of", np.round(scaleby, 5))
 
     # Make a synthetic spectrum based on the fluxes of S1723, redshifted and scaled
     startwave = 1300.  # Angstroms
     endwave   = 7000.  # Angstroms
-    Npix      = 4.0E4   #kludge
+    Npix      = 9.0E4   #kludge
     df_sim = pandas.DataFrame(data=pandas.Series(np.geomspace(startwave, endwave, Npix)), columns=('rest_wave',))
     df_sim['rest_flam'] = 0.0     # no continuum to start
     df_sim['rest_flam_u'] = 0.0   # no uncertainty either
     df_sim['d_restwave'] = df_sim['rest_wave'].diff()  # dlambda array, the width of 1 pixel
     df_sim['d_restwave'].iloc[0] = df_sim['d_restwave'].iloc[1] # first value is bogus
-    for row in detected_lines.itertuples() :  # Make rest frame synthetic spectrum.  Add a gaussian for each detected line, at the right flux
-        amplitude = scaleby * row.flux / row.sigma / np.sqrt(2.*np.pi)  
-        y_thisgauss = jrr.spec.onegaus(df_sim['rest_wave'],  amplitude, row.wave, row.sigma, 0.0)  # array to fill, AMPLITDE, central wavelength, sigma, continuum
-        df_sim['rest_flam'] +=  (y_thisgauss / df_sim['d_restwave'])  # per Angstrom
-
     detected_lines['obs_wave']    = detected_lines['wave'] * (1.0 + zz)
     detected_lines['scaled_flux'] = detected_lines['flux'] * scaleby   # Scale lineflux from above.  No (1+z), b/c flux is conserved
+    for row in detected_lines.itertuples() :  # Make rest frame synthetic spectrum.  Add a gaussian for each detected line, at the right flux
+        amplitude = row.scaled_flux / row.sigma / np.sqrt(2.*np.pi)  # scaleby already applied to scaled_flux.  No 1+z bc this is rest wave
+        y_thisgauss = jrr.spec.onegaus(df_sim['rest_wave'],  amplitude, row.wave, row.sigma, 0.0)  # array to fill, AMPLITUDE, central wavelength, sigma, continuum
+        df_sim['rest_flam'] +=  y_thisgauss
+
     jrr.spec.convert2obsframe_df(df_sim, zz, units='flam', colrw='rest_wave', colrf='rest_flam', colrf_u='rest_flam_u')
     df_sim['d_wave'] = df_sim['d_restwave'] * (1+zz)
     df_sim['wave_um'] = df_sim['wave']/1E4
     df_sim['fnu_mJy'] = jrr.spec.flam2fnu(df_sim['wave'], df_sim['flam']) /1E-23 *1E3 # convert cgs fnu to mJy
-    return( s1723_df, detected_lines, df_sim)
+    return( df_s1723, detected_lines, df_sim)
 
 def plot_synthetic_spectrum_flam(zz, sim_df, objname) :
     fig, ax1 = plt.subplots()
@@ -123,34 +123,45 @@ def plot_linefluxes_and_sens(zz, detected_lines, objname) :
     ax4.set_xlabel('rest wavelength (micron)')
     return(0)
 
+def test_flux_conservation(df_sim) :
+    print("Total flux in lines in rest-frame: ", '{:0.3e}'.format((df_sim.rest_flam * df_sim.d_restwave).sum()), "erg/s/cm^2")
+    print("Total flux in lines in obs-frame: ", '{:0.3e}'.format((df_sim.flam * df_sim.d_wave).sum()), "erg/s/cm^2")
+    return(0)
+
+def write_simulated_spectrum(outfilename, df_sim, zz, scaleby) :
+    df_sim.to_csv('temp', columns=('wave_um', 'fnu_mJy'), sep=' ', index=False)
+    header_text = "# Simulated spectrum at redshift zz=" + str(np.round(zz, 5)) + ", scaling from S1723 fluxes by a factor of " + str(np.round(scaleby, 5)) + "\n"
+    jrr.util.put_header_on_file('temp', header_text, outfilename)
+    return(0)
+
 ##################################################
+
 plt.close("all")
 
 # Simulate the z=5 COOL LAMPS arc
 zz_z5 = 5.0
-(s1723_df, dummy, dummy) = make_synthetic_spectrum_based_on_S1723(zz_z5, fwhm_kms=100, scaleby=1.0)  # Run this once, just to get the s1723_df spectrum to compute the real scaleby
-O2flux_s1723 = s1723_df[s1723_df['wave'].between(3725,3732)]['flux'].sum()   # Scale the line fluxes for the z=5 arc, linearly from flux of [O II] sum
+# For reference,    make_synthetic_spectrum_based_on_S1723 returns ( df_s1723, detected_lines, df_sim)
+(dum1, dum2, dum3) = make_synthetic_spectrum_based_on_S1723(zz_z5, fwhm_kms=10, scaleby=1.0)  # Run this once, just to get the df_s1723 spectrum to compute the real scaleby
+O2flux_s1723 = dum2[dum2['wave'].between(3725,3732)]['flux'].sum()   # Scale the line fluxes for the z=5 arc, linearly from flux of [O II] sum
 O2flux_z5  = 4.5E-16  # From Gourav, for z=5 source
 # Jane: the 16--84 percentile range is 2.48 to 6.57 X 10^-16 erg /s/cm^2/ w the median 4.5E-16
 scaleby_z5 = O2flux_z5 / O2flux_s1723 / 30.  # Scale to flux of z=5 source, and split into 30 pieces
-(s1723_df, z5_detected_lines, z5_sim) = make_synthetic_spectrum_based_on_S1723(5.0, fwhm_kms=100, scaleby=scaleby_z5)  # run again, but this time with scaleby
-outfilename = "sim_spectrum_z" + str(np.round(5.0, 2)) + "_v1.1.txt"
-z5_sim.to_csv(outfilename, sep=' ', columns=('wave_um', 'fnu_mJy'), index=False)
-
+(df_s1723, z5_detected_lines, z5_sim) = make_synthetic_spectrum_based_on_S1723(zz_z5, fwhm_kms=10, scaleby=scaleby_z5)  # run again, but this time with scaleby
+outfilename = "sim_spectrum_z" + str(np.round(zz_z5, 2)) + "_v1.1.txt"
+write_simulated_spectrum(outfilename, z5_sim, zz_z5, scaleby_z5)
 
 # Simulate S1110 at z=2.481.  Simply scale S1110 line fluxes from apparent magnitudes
-s1110_df =  load_s1110_Johnson_Tab4()  # load table 4 of Traci's paper.  Somethings weird about the fluxes.
+traci_tab4 =  load_s1110_Johnson_Tab4()  # load table 4 of Traci's paper.  Somethings weird about the fluxes. Ignore for now.
 zz_s1110 = 2.481                 # redshift from Traci's paper
 mAB_606 = 26.6  # AB magnitude apparent 606, from Matt on Slack
 Lnu = jrr.util.mAB_to_Lnu(mAB_606, zz_s1110)
 SFR = jrr.util.Kennicutt_LUV_to_SFR(Lnu)
 fHa  = jrr.util.Kennicutt_SFR_to_LHa(SFR) / (4. * np.pi * jrr.util.luminosity_distance(zz_s1110)**2)
 fHB = fHa / 3.0
-scaleby_s1110 =  fHB / s1723_df[s1723_df['wave'].between(4850, 4870)]['flux'].sum()
-(dummy2, s1110_detected_lines, s1110_sim) =  make_synthetic_spectrum_based_on_S1723(zz_s1110, fwhm_kms=100, scaleby=scaleby_s1110)  # run again, but this time with scaleby
+scaleby_s1110 =  fHB / df_s1723[df_s1723['wave'].between(4860, 4870)]['flux'].sum()
+(dummy3, s1110_detected_lines, s1110_sim) =  make_synthetic_spectrum_based_on_S1723(zz_s1110, fwhm_kms=10, scaleby=scaleby_s1110)  
 outfilename = "sim_spectrum_z" + str(np.round(zz_s1110, 2)) + "_v1.1.txt"
-s1110_sim.to_csv(outfilename, sep=' ', columns=('wave_um', 'fnu_mJy'), index=False)
-
+write_simulated_spectrum(outfilename, s1110_sim, zz_s1110, scaleby_s1110)
 
 
 plot_synthetic_spectrum_flam(zz_z5, z5_sim, "z5")
@@ -159,13 +170,10 @@ plot_linefluxes_and_sens(zz_z5, z5_detected_lines, 'z5')
 plot_synthetic_spectrum_flam(zz_s1110, s1110_sim, 's1110')
 plot_linefluxes_and_sens(zz_s1110, s1110_detected_lines, 's1110')
 
+print("Testing flux conservation.  Total lineflux in obs, rest frames should match:")
+test_flux_conservation(z5_sim)
+test_flux_conservation(s1110_sim)
 
 
-
-test_flux_conservation = True
-if test_flux_conservation :
-    print("Total flux in lines in rest-frame was", '{:0.3e}'.format((z5_sim.rest_flam * z5_sim.d_restwave).sum()), "erg/s/cm^2")
-    print("Total flux in lines in obs-frame  was", '{:0.3e}'.format((z5_sim.flam * z5_sim.d_wave).sum()), "erg/s/cm^2")
-    print("These should match because flux conservation.")
 
 plt.show()
